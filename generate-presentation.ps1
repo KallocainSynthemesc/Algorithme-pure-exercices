3b12bbc9176aeb72daa6c35871b4d8cbb4f5034c
KallocainSynthemesc <74613762+KallocainSynthemesc@users.noreply.github.com>
2022-10-19 08:56:36 +0200

    first commit

---
+++ generate-presentation.ps1
@@ -0,0 +1,9 @@
+﻿$BackupEncoding = [Console]::OutputEncoding
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$dir = (Get-Location).tostring() + "\build"
+[System.IO.Directory]::CreateDirectory($dir)
+
+$index = $dir + "\index.html"
+inliner index.html | Out-File -FilePath $index
+
+[Console]::OutputEncoding = $BackupEncoding
